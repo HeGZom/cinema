31b3452ed5927eec1e539b5fd6ffdf579eaa869a
Gleb <HeGZom@hegzom@mail.ru>
2024-12-21 22:57:18 +0700

    change

--- cinema/cinema/admin.py
+++ cinema/cinema/admin.py
@@ -1,3 +1,22 @@
 # from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Hall, Movie, Session, Seat, isReservation
+
+@admin.register(Hall)
+class HallAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    pass
+@admin.register(isReservation)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
